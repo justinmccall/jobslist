8c139b8e2a0d6701dc4a4efabfa50b80b0dfd83c
justinmccall <justin@resonate.co.za>
2024-07-22 09:42:26 +0400

    Add files via upload

---
+++ jobs.sql
@@ -0,0 +1,12 @@
+-- -------------------------------------------------------------
+-- TablePlus 6.1.2(568)
+--
+-- https://tableplus.com/
+--
+-- Database: jobs.db
+-- Generation Time: 2024-07-22 09:42:18.4600
+-- -------------------------------------------------------------
+
+
+CREATE TABLE "jobs" ("id" integer,"position" text,"company" text,"companyLogo" text,"location" text,"date" integer,"agoTime" text,"salary" text,"jobUrl" text,"source" text,"description" text,"easy_apply" integer,"apply_url" text,"status" text,"work_type" text,"sector" text,"updatedDate" integer);
+
