8befb9b49f6bf940653bac1f168989de4ce24a8e
Justin <justin@resonate.co.za>
2024-07-22 11:01:18 +0400

    update

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors','1');
+
+$mysqli = mysqli_connect("localhost","root","#&TRFhsG6S@D7#EP","jobs");
+// $mysqli = mysqli_connect("localhost","416e90a56710","8e0968dd9a18d38e","jobs");
+
+
+?>
