e712cfe3708227df0ea036b58bea421128235ed3
justinmccall <justin@resonate.co.za>
2024-07-22 09:53:48 +0400

    Update jobs.sql

--- jobs.sql
+++ jobs.sql
@@ -1,12 +1,19 @@
--- -------------------------------------------------------------
--- TablePlus 6.1.2(568)
---
--- https://tableplus.com/
---
--- Database: jobs.db
--- Generation Time: 2024-07-22 09:42:18.4600
--- -------------------------------------------------------------
-
-
-CREATE TABLE "jobs" ("id" integer,"position" text,"company" text,"companyLogo" text,"location" text,"date" integer,"agoTime" text,"salary" text,"jobUrl" text,"source" text,"description" text,"easy_apply" integer,"apply_url" text,"status" text,"work_type" text,"sector" text,"updatedDate" integer);
-
+CREATE TABLE `jobs` (
+  `id` INT,
+  `position` TEXT,
+  `company` TEXT,
+  `companyLogo` TEXT,
+  `location` TEXT,
+  `date` DATE,
+  `agoTime` TEXT,
+  `salary` TEXT,
+  `jobUrl` TEXT,
+  `source` TEXT,
+  `description` TEXT,
+  `easy_apply` TINYINT(1),
+  `applicationUrl` TEXT,
+  `applicationStatus` TEXT,
+  `work_type` TEXT,
+  `sector` TEXT,
+  `updateDate` DATE
+);
